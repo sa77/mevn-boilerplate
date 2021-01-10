ae3bc4283188babc03ebf2cad10a6200733608d2
Sandeep Acharya <acharya877@gmail.com>
2021-01-10 14:25:55 +1100

    Add base64 encoder for jwt

--- api/src/utils/constants.ts
+++ api/src/utils/constants.ts
@@ -2,4 +2,4 @@ export const PORT: number | string = process.env.PORT || 8000;
 
 export const MONGODB_URL:string = process.env.MONGODB_URL || "mongodb://localhost/test"
 
-export const JWT_SIGNING_SECRET = "c9e2361152a673800aca394ae18d4231e63abc50fa7c2c168d059390eddd5d64"
+export const JWT_SIGNING_SECRET = "7547964de6843c013a5c12900f8015b1fe949e3b5a4ca8c21ed03ed5cc95a2f322903305eea67c47962d65119b9c344d93a9fe162d7a721fca1d2d5f6dc537c4"
